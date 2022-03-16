8a94c54290c24dcbc518669b3a7fa4cc206656b3
LiviuAC <liviu.celcan@softvision.com>
2022-03-16 18:24:20 +0200

    added dataModel and modified the config file, several pageobjects and specs

---
+++ test/helper/dataModel.js
@@ -0,0 +1,26 @@
+exports.ImageSource = {
+    backpack: '/static/media/sauce-backpack-1200x1500.34e7aa42.jpg',
+    bikeLight: '/static/media/bike-light-1200x1500.a0c9caae.jpg',
+    boltShirt: '/static/media/bolt-shirt-1200x1500.c0dae290.jpg',
+    fleeceJacket: '/static/media/sauce-pullover-1200x1500.439fc934.jpg',
+    onesie: '/static/media/red-onesie-1200x1500.1b15e1fa.jpg',
+    redShirt: '/static/media/red-tatt-1200x1500.e32b4ef9.jpg'
+}
+
+exports.ProductNames = {
+    backpack: 'Sauce Labs Backpack',
+    bikeLight: 'Sauce Labs Bike Light',
+    boltShirt: 'Sauce Labs Bolt T-Shirt',
+    fleeceJacket: 'Sauce Labs Fleece Jacket',
+    onesie: 'Sauce Labs Onesie',
+    redShirt: 'Test.allTheThings() T-Shirt (Red)'
+}
+
+exports.ProductPrices = {
+    backpack: '$29.99',
+    bikeLight: '$9.99',
+    boltShirt: '$15.99',
+    fleeceJacket: '$49.99',
+    onesie: '$7.99',
+    redShirt: '$15.99'
+}
